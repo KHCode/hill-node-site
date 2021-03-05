f463448f5be82c8a6a4ee71b0cb73af67554b288
KHCode <hillkri@oregonstate.edu>
2021-03-05 13:44:50 -0800

    added major components to views + css

---
+++ public/javascripts/frontEndHelpers.js
@@ -0,0 +1,15 @@
+module.exports.getDetails = function () {
+  let card = document.querySelector('.portfolio-card');
+
+  let body = document.querySelector('.portfolio-card__body');
+  
+  console.log(body);
+
+  card.onclick = () => {
+    // card.classList.toggle('show-details');
+    body.classList.toggle('reveal');
+    console.log(body);
+  }
+
+  return card.onclick;
+}
