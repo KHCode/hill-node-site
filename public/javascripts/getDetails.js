81187d7303b08cf126cf567330353fe9e9740ac4
KHCode <hillkri@oregonstate.edu>
2021-03-14 10:27:50 -0700

    added link buttons to portfolio cards

--- public/javascripts/getDetails.js
+++ public/javascripts/getDetails.js
@@ -1,29 +1,12 @@
-// module.exports.getDetails = function () {
-//   let card = document.querySelector('.portfolio-card');
-
-//   let body = document.querySelector('.portfolio-card__body');
-  
-//   console.log(body);
-
-//   card.onclick = () => {
-//     // card.classList.toggle('show-details');
-//     body.classList.toggle('reveal');
-//     console.log(body);
-//   }
-// }
-
-// function getDetails() {
-  let cards = document.querySelectorAll('.portfolio-card').forEach((card) => {
-
-    card.onclick = () => {
-      // card.classList.toggle('show-details');
-      let body = card.querySelector('.portfolio-card__body');
-      console.log(body);
-      body.classList.toggle('reveal');
-    }
-  });
+let cards = document.querySelectorAll('.portfolio-card').forEach((card) => {
+  let infoButton = card.querySelector('.more-info');
+  infoButton.onclick = () => {
+    let body = card.querySelector('.portfolio-card__body');
+    console.log(body);
+    body.classList.toggle('reveal');
+  }
+});
   
   
 
 
-// }
